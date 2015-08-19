b8fdc9a7701aa5dcf7c8c47adc8ec123364fb915
d5039m <d5039m@users.noreply.github.com>
2015-08-19 12:27:22 +0100

    add radioTest.py to perform simple radio broadcast

---
+++ habProject/balloon/scripts/radioTest.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import serial
+import time
+
+print 'Opening serial port to radio, 300bps, 8bit, no parity, two stop'
+ser = serial.Serial('/dev/ttyAMA0',300, serial.EIGHTBITS, serial.PARITY_NONE, serial.STOPBITS_TWO)
+
+try:
+    while True:
+        print("Sending Radio Beacon message")
+        ser.write("RADIO BEACON TEST")
+	      time.sleep(1)
+        
+except KeyboardInterupt:
+    print "Program shutdown from keyboard"
+	
+finally:
+    ser.close()     
